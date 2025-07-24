e6f0325140a7df269b08c3b9e27612cf696358f3
DaviF <davifernandesdosreis14@gmail.com>
2025-07-24 10:10:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,46 +10,49 @@ function jump() {
     if (isJumping) return; 
     isJumping = true;
     dino.style.animation = "jump 0.5s ease-out";
-    
+
     setTimeout(() => {
         dino.style.animation = "";
         isJumping = false;
     }, 500);
 }
+
 document.addEventListener("keydown", function(event) {
-    if (event.code === "Space") { 
+    if (event.code === "Space" || event.key === " ") { 
         jump();
     }
 });
 
- function moveObstaculo(){
+function moveObstaculo() {
     let obstaculoPos = obstaculo.offsetLeft;
     obstaculo.style.left = obstaculoPos - speed + "px";
+
+    checkCollision(); // Corrigido: verificar a colisão a cada movimento
+
     if (obstaculoPos <= -20) {
         obstaculo.style.left = gameAreaWidth + "px";
-        checkCollision();
     }
- }
+}
 
- function checkCollision() {
- const dinoRect = dino.getBoundingClientRect();
- const obstaculoRect = obstaculo.getBoundingClientRect();
+function checkCollision() {
+    const dinoRect = dino.getBoundingClientRect();
+    const obstaculoRect = obstaculo.getBoundingClientRect();
 
-    if(
+    if (
         dinoRect.right > obstaculoRect.left &&
-        dinoRect.left  < obstaculoRect.right&&
+        dinoRect.left < obstaculoRect.right &&
         dinoRect.bottom > obstaculoRect.top &&
         dinoRect.top < obstaculoRect.bottom
-    ){
-        alert("parabens!, voce perdeu para um cacto...tente novamente e brilhe!")
+    ) {
+        alert("parabéns!, você perdeu para um cacto... tente novamente e brilhe!");
         clearInterval(gameInterval);
         clearInterval(speedInterval);
-         }
     }
+}
 
-    function increaseSpeed(){
-        speed += acceleration;
-    }
+function increaseSpeed() {
+    speed += acceleration;
+}
 
-    const speedInterval = setInterval(increaseSpeed,30000);
-    const gameInterval = setInterval (moveObstaculo, interval);
+const speedInterval = setInterval(increaseSpeed, 30000);
+const gameInterval = setInterval(moveObstaculo, interval);
